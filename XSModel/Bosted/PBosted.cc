aaaf0ab3b7c9457cda1afecd18b0db7c9d160d4b
Jixie Zhang <jixie@jlab.org>
2021-06-09 16:48:37 -0400

    replace F1F209 by F1F221 in XSModel

--- XSModel/Bosted/PBosted.cc
+++ XSModel/Bosted/PBosted.cc
@@ -13,13 +13,70 @@
 //for heavier nuclei. It works well for 4He, but the model for 3He still needs tweaking. 
 //The reference for the 2009 $A>2$ fit is: http://arxiv.org/abs/1203.2262. 
 //
+/* F1F221Wrapper
+ * Unit of xs is ub/MeV-sr.
+ * F1F209.f is valid for all W<5 GeV and all Q2<11 GeV2.
+ * Inside the code of F1F209.f, I saw that this fit does not work for Q2>11. 
+ * https://github.com/jixie/CreateXSTree/blob/main/XSModel/Bosted/bosted.f#L1010
+ * For A>2, it used function fitemc() , which is a fit to "EMC effect". 
+ * fitemc() marks a fit to good_fit only if 0.0085< Xbj <0.7.
+ * https://github.com/jixie/CreateXSTree/blob/main/XSModel/Bosted/bosted.f#L1696
+ *
+ * In F1F221.f, Q2 and W2 have been extended up to 32.
+ * For A > 2, it does not use fitemc() any longer. It uses the parameterization  based on
+ * 12C, 27Al, 56Fe and 64Cu.
+ * It claims that this fit scales relatively well for all nuclei with 10 < A < 80.
+ *
+ * Use -fno-leading-underscore -fno-second-underscore when compiling F1F221.f
+ */
+#include <math.h>
+#include <iostream>
 
 namespace PBosted 
 {
+	static const bool kUseF1F2Version21 = true;
+
 	extern "C" 
 	{
 		void bosted_(double* Z, double* A, double* Ei, double* Ep, double* ang, double* xs, double *Tb, double *Ta);
+		
+		void f1f2in21_(double* Z, double* A, double* Q2, double* W2, double* F1, double* F2);
+		void f1f2qe21_(double* Z, double* A, double* Q2, double* W2, double* F1, double* F2);
 	}
+
+
+	double GetXS21(double Z, double A, double Ei, double Ef, double theta)
+	{
+		const double M = 0.93825;
+		theta = fabs(theta);  //in case the theta is negative 
+		double halftheta = theta/2;
+
+		double nu = Ei - Ef;
+		double Q2 = 4. * Ei * Ef * pow(sin(halftheta), 2.0);
+		double w2 = M * M + 2. * M * nu - Q2;
+
+		double F1, F2;
+		double xs1, xs2;
+
+		//original code
+      	//xs2=(2./137.*Ep/q2*cos(theta/2))**2. !mott
+      	//xs2=xs2*(2/M*F1*tan(abs(theta)/2)**2+F2/nu)
+      	//xs2=xs2*389.379
+
+		f1f2in21_(&Z, &A, &Q2, &w2, &F1, &F2);
+		xs1 = pow(2. / 137. * Ef / Q2 * cos(halftheta), 2.0); // mott
+		xs1 = xs1 * (2. / M * F1 * pow(tan(halftheta), 2.0) + F2 / nu);
+		xs1 = xs1 * 389.379;
+
+		f1f2qe21_(&Z, &A, &Q2, &w2, &F1, &F2);
+		xs2 = pow(2. / 137. * Ef / Q2 * cos(halftheta), 2.0); // mott
+		xs2 = xs2 * (2. / M * F1 * pow(tan(halftheta), 2.0) + F2 / nu);
+		xs2 = xs2 * 389.379;
+
+		//std::cout<<" Theta="<<theta*57.3<<"  XS_in="<<xs1<<"  XS_qe="<<xs2<<std::endl; 
+		return (xs1 + xs2) / 1000.; // ub/MeV-sr
+	}
+
 
 	//input
 	//       Z,N: proton and neutron number of the nucleus.	;
@@ -38,7 +95,12 @@ namespace PBosted
 		//this fortran routine does not work in windows, do not know why
 		return 1.0;
 #else
-		bosted_(&NZ, &NA, &Ei, &Ef, &theta, &XS, &Tb, &Ta);
+		if(kUseF1F2Version21) {
+			XS = GetXS21(NZ, NA, Ei, Ef, theta);
+		}
+		else {
+			bosted_(&NZ, &NA, &Ei, &Ef, &theta, &XS, &Tb, &Ta);  //rad corr does not work in this version, no time to check it yet
+		}
 #endif
 		return XS;
 	}
--- XSModel/Bosted/PBosted.hh
+++ XSModel/Bosted/PBosted.hh
@@ -13,6 +13,24 @@
 //for heavier nuclei. It works well for 4He, but the model for 3He still needs tweaking. 
 //The reference for the 2009 $A>2$ fit is: http://arxiv.org/abs/1203.2262. 
 //
+/* add F1F221Wrapper
+ * Unit of xs is ub/MeV-sr.
+ * F1F209.f is valid for all W<5 GeV and all Q2<11 GeV2.
+ * Inside the code of F1F209.f, I saw that this fit does not work for Q2>11. 
+ * https://github.com/jixie/CreateXSTree/blob/main/XSModel/Bosted/bosted.f#L1010
+ * For A>2, it used function fitemc() , which is a fit to "EMC effect". 
+ * fitemc() marks a fit to good_fit only if 0.0085< Xbj <0.7.
+ * https://github.com/jixie/CreateXSTree/blob/main/XSModel/Bosted/bosted.f#L1696
+ *
+ * In F1F221.f, Q2 and W2 have been extended up to 32.
+ * For A > 2, it does not use fitemc() any longer. It uses the parameterization  based on
+ * 12C, 27Al, 56Fe and 64Cu.
+ * It claims that this fit scales relatively well for all nuclei with 10 < A < 80.
+ *
+ * Use -fno-leading-underscore -fno-second-underscore when compiling F1F221.f
+ */
+
+
 #ifndef _PBosted_H
 #define _PBosted_H
 
@@ -27,7 +45,9 @@ namespace PBosted
 	//Tb: material thickness in unit of radiation length before scattering;
 	//Ta: material thickness in unit of radiation length after scattering;
 
-double GetXS(int Z, int N, double Ei, double Ef, double theta, double Tb=-0.001, double Ta=-0.001);
+	double GetXS(int Z, int N, double Ei, double Ef, double theta, double Tb=-0.001, double Ta=-0.001);
+
+	double GetXS21(double Z, double A, double Ei, double Ef, double theta);  //use F1F221.f
 }
 #endif
 
